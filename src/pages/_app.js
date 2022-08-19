778bfdd22e2905a92f4b82ed3f3fc727b25959cd
André Padez <andre.padez@gmail.com>
2022-08-19 18:18:01 +0100

    gif and png

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import 'strooks/dist/styles.scss'
+import 'styles/strooks-extend.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,78 @@
-import { useEffect, useState } from 'react'
-import { TypeWriter } from 'strooks'
+import { useEffect, useState, useRef } from 'react'
+import { TypeWriter, IconMaterial } from 'strooks'
+import { MdFullscreen, MdFullscreenExit } from 'react-icons/md'
+import useFullscreen from 'useFullscreen'
 import Image from 'next/image'
+import Head from 'next/head'
 
 export default function Home() {
+  const gifRef = useRef()
+  const { fullscreen, isFullscreen, exitFullscreen } = useFullscreen(gifRef)
   const [isSmallScreen, setIsSmallScreen] = useState(false)
+  const originalGifSize = { width: 960, height: 600 }
+  const [gifSize, setGifSize] = useState(originalGifSize)
+
+  useEffect(() => {
+    if (isFullscreen) {
+      const width = window.innerWidth
+
+      const height = (originalGifSize.height * window.innerWidth) / originalGifSize.width
+      setGifSize({ width, height })
+    } else {
+      setGifSize(originalGifSize)
+    }
+  }, [isFullscreen])
+
   useEffect(() => {
     if (window.innerWidth < 576) {
       setIsSmallScreen(true)
     }
     setTimeout(() => document.getElementById('section-1').scrollIntoView(), 7 * 1000)
   }, [])
+
+  const { width, height } = gifSize
+
   return (
     <div className="container bg-black white" style={{ overflow: 'hidden' }}>
       <section id="section-0" className="flex center vh-100 bg-black white">
         <div className="text-center flex column gap-5">
           <TypeWriter text={headerText} size={35 * (isSmallScreen ? 0.7 : 1)} time={2} clearBlink />
-          <TypeWriter text={middleText} size={25 * (isSmallScreen ? 0.8 : 1)} time={2} delay={2} clearBlink />
+          <TypeWriter
+            text={middleText}
+            size={25 * (isSmallScreen ? 0.8 : 1)}
+            time={2}
+            delay={2}
+            clearBlink
+          />
           <TypeWriter text={subText} size={20 * (isSmallScreen ? 0.8 : 1)} time={2} delay={4} />
         </div>
       </section>
-
-      <section id="section-1" className="flex column gap-5 center vh-100 bg-black white">
-        <h1>everything is easy with Strooks</h1>
-        <Image src="/gifs/00-strooks-flash-intro.gif" width={960} height={600} />
+      <Head>
+        <link rel="preload" as="image" href="/gifs/00-strooks-flash-intro.mp4" />
+      </Head>
+      <section
+        id="section-1"
+        className="flex center gap-5 column text-center relative center vh-100 "
+      >
+        <h1 className="">everything is easy with Strooks</h1>
+        <div ref={gifRef} className="relative">
+          {isSmallScreen ? (
+            <Image src="/gifs/00-strooks-flash-intro.png" width={550} height={584} />
+          ) : (
+            <Image src="/gifs/00-strooks-flash-intro.gif" width={width} height={height} />
+          )}
+          <div className="absolute bottom right pr-3 pb-3 scale-15 teal pointer">
+            {!isSmallScreen && (
+              <h1>
+                {isFullscreen ? (
+                  <MdFullscreenExit onClick={exitFullscreen} />
+                ) : (
+                  <MdFullscreen onClick={fullscreen} />
+                )}
+              </h1>
+            )}
+          </div>
+        </div>
       </section>
     </div>
   )
